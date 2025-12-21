0316f36823870cc33555ec80d382bf7446c7dfb2
AgonSelmani <agonselmani1@gmail.com>
2025-12-21 18:37:03 +0100

    Add scroll up button and theme toggle functionality

--- assets/js/main.js
+++ assets/js/main.js
@@ -69,3 +69,59 @@ function scrollActive() {
 }
 
 window.addEventListener("scroll", scrollActive);
+
+/*SHOW SCROLL UP */
+function scrollUP() {
+    const scrollUp = document.getElementById('scroll-up');
+
+    if (window.scrollY >= 350) {
+        scrollUp.classList.add('show-scroll');
+    } else {
+        scrollUp.classList.remove('show-scroll');
+    }
+}
+
+window.addEventListener('scroll', scrollUP);
+
+const scrollUpButton = document.getElementById('scroll-up');
+scrollUpButton.addEventListener('click', (event) => {
+    event.preventDefault();
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+});
+
+
+/* DARK LIGHT THEME */
+
+const themeButton = document.getElementById('theme-button');
+const darkTheme = 'dark-theme';
+const iconTheme = 'bx-sun';
+
+const toggleTheme = () => {
+    document.body.classList.toggle(darkTheme);
+    themeButton.classList.toggle(iconTheme);
+
+    localStorage.setItem('selected-theme', document.body.classList.contains(darkTheme) ? 'dark' : 'light');
+    localStorage.setItem('selected-icon', themeButton.classList.contains(iconTheme) ? 'bx bx-moon' : 'bx bx-sun');
+};
+
+const selectedTheme = localStorage.getItem('selected-theme');
+const selectedIcon = localStorage.getItem('selected-icon');
+
+if (selectedTheme) {
+    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme);
+    themeButton.classList[selectedIcon === 'bx bx-moon' ? 'add' : 'remove'](iconTheme);
+}
+
+themeButton.addEventListener('click', toggleTheme);
+
+document.querySelectorAll('.popup-enabled').forEach((element)=>{
+    element.addEventListener('click', (event)=>{
+        event.preventDefault();
+        console.log(event.target);
+        alert('Buttons and links aren\'t functional.' + event.target)
+    })
+})
+
